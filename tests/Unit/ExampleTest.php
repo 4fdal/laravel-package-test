f01ca24df7e519578c8d6f7570272a50043e843a
afdal-develop <afdal-develop@gmail.com>
2022-02-28 22:22:36 +0700

    first commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace KodeIngatan\Mudahsenyo\Unit;
+
+use KodeIngatan\Mudahsenyo\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
